78da352647218ed2f1d0549f6f113c1ff62baf51
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-02-20 18:36:11 +0530

    app;y active class and and hover effect

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 
 
 $(document).ready(function(){ 
-  $(" .accordin-title").click(function(){
-   $(this).closest(".accordin-wrapper").find(".accordin-content").slideToggle();
-   $(this).closest(".accordin-wrapper").siblings().find(".accordin-content").slideUp();
-   $(this).closest(".accordin-wrapper").find(".accordin-items").toggleClass("active");
-   $(this).closest(".accordin-wrapper").siblings().find(".accordin-items").removeClass("active")
+  $(" .accordin-title.page-wrapper .side-navigation .logout-section a").click(function(){
+    alert();
+   $(this).closest(".nav-listing").find("li").toggleClass("active");
+   $(this).closest(".nav-listing").siblings().find(".li").removeClass("active")
   });
 });
