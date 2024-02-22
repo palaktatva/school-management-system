190d101e7cbabe30881c92cf63d0f9207f1be2a2
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-02-22 18:32:11 +0530

    design table section and humbuerger user-image-changes-pendding

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
+jQuery(document).ready(function(){
+  jQuery('.hamburger-icon').click(function () {
+      jQuery('body').toggleClass('open-menu');
+    }); 
+})
 
-
-$(document).ready(function(){ 
-  $(" .accordin-title.page-wrapper .side-navigation .logout-section a").click(function(){
-    alert();
-   $(this).closest(".nav-listing").find("li").toggleClass("active");
-   $(this).closest(".nav-listing").siblings().find(".li").removeClass("active")
-  });
-});
