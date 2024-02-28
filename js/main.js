bb33c71deb6652cb107dcd94fcf30b967b62070c
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-02-28 18:13:43 +0530

    apply custom-model in logout section

--- js/main.js
+++ js/main.js
@@ -7,13 +7,37 @@ document.write(
 
 jQuery(document).ready(function(){
 
-    $('.week-selection').select2({
+    jQuery('.week-selection').select2({
       minimumResultsForSearch: Infinity
     });
 
   jQuery('.hamburger-icon, .cross-icon').click(function () {
       jQuery('body, html').toggleClass('open-menu');
     }); 
+
+    jQuery('.modal-btn').click(function () {
+      jQuery("body,html").addClass('modal-open');
+      var _this = jQuery(this).attr('data-link');
+
+      var _currentModal = jQuery(".custom-modal[data-target='" + _this + "']");
+      _currentModal.addClass("visible");
+
+      setTimeout(function () {
+          _currentModal.addClass("fadein");
+      }, 125);
+  });
+  jQuery('.custom-modal,.modal-close').click(function () {
+      jQuery("body,html").removeClass('modal-open');
+      var _this = jQuery(this)
+      jQuery(this).closest(".custom-modal").removeClass("fadein");
+      setTimeout(function () {
+          _this.closest(".custom-modal").removeClass("visible");
+      }, 125);
+
+  });
+  jQuery(".custom-modal .modal-inner-content").click(function(e){
+      e.stopPropagation();
+  })
     
 })
 
