cd5ac785fde11afbb9dc5166eed31c9bf1989545
Palak Sidhdhapura <pcs135@sparsh1.com>
2024-03-01 18:33:17 +0530

    design message section

--- js/main.js
+++ js/main.js
@@ -38,6 +38,20 @@ jQuery(document).ready(function(){
   jQuery(".custom-modal .modal-inner-content").click(function(e){
       e.stopPropagation();
   })
+
+
+  
+  jQuery(".custom-tab-wrapper .tab-heading-block ul li a").click(function (e) {
+    e.preventDefault();
+    var _link = jQuery(this).attr('data-link');
+    jQuery(this).closest('li').addClass('active');
+    jQuery(this).closest('li').siblings().removeClass('active');
+    jQuery(this).closest('.custom-tab-wrapper').find('.tab-inner-content[data-tab="'+_link+'"]').addClass('active');
+    jQuery(this).closest('.custom-tab-wrapper').find('.tab-inner-content[data-tab="'+_link+'"]').siblings().removeClass('active');
+  });
+
     
-})
+  })
+    
+
 
